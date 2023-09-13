c49c73628100cde56239049680536b63fe1e6a82
Bao Nguyen <bao.nguyen2@trustingsocial.com>
2023-09-13 18:02:48 +0700

    trigger deploy

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -6,6 +6,7 @@ import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
 export default () => {
+  console.log('ss');
   return (
     <div>
       <StylesProvider>
