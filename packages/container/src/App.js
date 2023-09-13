10ad3bfb0233f8dcbc18fe31f1c4ace4ab2ee356
Bao Nguyen <bao.nguyen2@trustingsocial.com>
2023-09-13 17:15:47 +0700

    re-trigger deploy

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
 export default () => {
+  console.log('test change');
   return (
     <>
       <h1>Hi there!</h1>
