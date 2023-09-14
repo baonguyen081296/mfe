5280896f5beedf28eae22d1e0de3062a5b049503
Bao Nguyen <bao.nguyen2@trustingsocial.com>
2023-09-14 10:30:38 +0700

    feature/test

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
 export default () => {
+  console.log('feature test');
   return (
     <>
       <h1>Hi there!</h1>
