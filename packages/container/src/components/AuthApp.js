0f70c540fdde03c0b498a25d1f15a6ad3a8b3f6c
Bao Nguyen <bao.nguyen2@trustingsocial.com>
2023-10-09 16:06:26 +0700

    update

--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -1,23 +1,25 @@
-// import { mount } from 'auth/AuthApp';
+import { mount } from 'auth/AuthApp';
 import React, { useRef, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
-export default () => {
+export default ({ onSignIn }) => {
   const ref = useRef(null);
   const history = useHistory();
 
   useEffect(() => {
-    // const { onParentNavigate } = mount(ref.current, {
-    //   onNavigate: ({ pathname: nextPathname }) => {
-    //     const { pathname } = history.location;
+    const { onParentNavigate } = mount(ref.current, {
+      initialPath: history.location.pathname,
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location;
 
-    //     if (pathname !== nextPathname) {
-    //       history.push(nextPathname);
-    //     }
-    //   },
-    // });
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+      onSignIn,
+    });
 
-    // history.listen(onParentNavigate);
+    history.listen(onParentNavigate);
   }, []);
 
   return <div ref={ref} />;
